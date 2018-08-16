726e0b28c8c5370c411dd1f4eb856c4beab20e60
Alexandra <Lexxiecole@gmail.com>
2018-08-16 12:34:08 -0400

    psuedo code started

---
+++ javascript/game.js
@@ -0,0 +1,15 @@
+//psuedo code for wins:
+    // a counter needs to be installed
+    // an if/then statement needs to be made.
+    // a random letter generator needs to be made
+
+//psuedo code for losses:
+    // a counter needs to be installed
+    // an if/then statement needs to be made.
+
+
+//psuedocode for guesses left:
+    // a loop needs to be made
+
+//psuedo code for guesses made:
+    // a loop needs to be made
