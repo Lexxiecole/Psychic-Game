a3f1271254ef5d37ce7f13b54734042aac351442
Alexandra <Lexxiecole@gmail.com>
2018-08-16 17:39:37 -0400

    added javascript and new psuedo code. need to next link the html text with the javascript

--- javascript/game.js
+++ javascript/game.js
@@ -1,50 +1,61 @@
-//psuedo code for wins:
-    // a counter needs to be installed
-    // an if/then statement needs to be made.
-    // a random letter generator needs to be made
 
-var wins=confirm("confirm?"); //creating my var here
+
+//creating my var here
+var wins=0; 
+
+//creating my var here
+var losses=0;  
+
+//creating var
+var left=9;
+
+//creating var
+var made=0;   
+
+
+
+    // a random letter array needs to be made
+    var rando = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]   
+
+    //computer needs to choose random letter
+    var computerGuess = rando[Math.floor(Math.random() * rando.length)];
+
+    //user guesses a letter
+    var userGuess = event.key
+
+    //user guesses letter correctly--must create a loop
+    if (userGuess === computerGuess) {
+        wins = ++;
+        userGuess = 9;
+    }
+
+    //user guesses letter incorrectly
+    if (userGuess != computerGuess) {
+        losses = ++;
+        userGuess = 9;
+    }
+
+
+
 
 
 //psuedo code for losses:
     // a counter needs to be installed
     // an if/then statement needs to be made.
 
-var losses=confirm("losses");  //creating my var here
+
 
 
 //psuedocode for guesses left:
     // a loop needs to be made
 
-var left=confirm("confirm");     //creating var
+
+
 var left = i=0; i++               //i know i need i++ becuase i need to add one every time a new letter is guessed
                                 //I need to use the i++ loop to end the number of guesses.
 
                                 //if I > 6, restart?
 
 
-
-
-
-
-
-
 //psuedo code for guesses made:
     // a loop needs to be made
-
-var made=confirm("confirm");     //creating var
-
-
-
-//practing 
-function makeid() {
-    var text = "";
-    var possible = "acdefghijklmnopqrstuvwxyz"; //to generate a string, there has to be quotes between it. so this isnt correct.
-  
-    for (var i = 0; i < 5; i++)
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-  
-    return text;
-  }
-  
-  console.log(makeid());
